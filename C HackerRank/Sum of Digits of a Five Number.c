7521c46835d5100cb53bc6b4efee5b02a4339d9c
SJCET5746 <145741327+sjcet5746@users.noreply.github.com>
2024-09-01 14:15:22 +0530

    Add files via upload

---
+++ C HackerRank/Sum of Digits of a Five Number.c
@@ -0,0 +1,17 @@
+//Written by Gskd
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    int num, sum = 0;
+    scanf("%d", &num);
+    while(num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    printf("%d", sum);
+}
